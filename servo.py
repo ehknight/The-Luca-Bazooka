407ca334ba6079f57a7648c6cd796ec6a3136ad8
Ethan <ethlu@github.com>
2016-02-02 10:27:52 -0800

    Updated servo.py

--- servo.py
+++ servo.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 
 class Servo:
-    def __init__(self, pin_horizon, pin_vertical):
+    def __init__(self, pin_horizon, pin_vertical,xconst,yconst):
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(pin_horizon,GPIO.OUT)
         GPIO.setup(pin_vertical,GPIO.OUT)
@@ -12,9 +12,19 @@ class Servo:
         self.dc_vertical = 100
         pwm_horizon.start(dc_horizon)
         pwm_vertical.start(dc_vertical)
-        
+        self.xconst = xconst
+        self.yconst = yconst
+
     def write_horizon(self,dc):
         self.pwm_horizon.ChangeDutyCycle(dc)
 
     def write_vertical(self, dc):
-        self.
+        self.pwm_vertical.ChangeDutyCycle(dc)
+
+    def update_dx(self, dx):
+        self.write_horizon(self.dc_horizon+dx*self.xconst)
+
+    def update_dy(self, dy):
+        self.write_vertical(self.dc_vertical+dy*self.yconst)
+
+
